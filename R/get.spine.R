3487fa4f50d79151a2bdf9576e4a94b1b6cdff47
mklau <matthewklau@fas.harvard.edu>
2018-03-12 16:38:39 -0400

    fixes get.spine

--- R/get.spine.R
+++ R/get.spine.R
@@ -13,26 +13,8 @@
 #' @importFrom igraph bfs
 #' @importFrom igraph graph_from_adjacency_matrix
 
-get.spine <- function(node.id,g){
+
+get.spine <- function(node.id, g){
     ig <- graph_from_adjacency_matrix(g)
-    nodes <- names(na.omit(bfs(ig, 
-                               root = node.id, 
-                               neimode = c('in'),
-                               unreachable = FALSE)$order))
-    if (length(nodes) == 1 & any(grepl("d",nodes))){
-        nodes <- names(na.omit(bfs(ig, 
-                                   root = node.id, 
-                                   neimode = c('out'),
-                                   unreachable = FALSE)$order))
-        nodes <- grep("p", nodes, value = TRUE)
-        nodes <- paste0("p",sort(as.numeric(do.call(rbind,strsplit(nodes,"p"))[,2])))
-    }else{
-        nodes <- names(na.omit(bfs(ig, 
-                                   root = node.id, 
-                                   neimode = c('in'),
-                                   unreachable = FALSE)$order))
-        nodes <- grep("p", nodes, value = TRUE)
-        nodes <- paste0("p",sort(as.numeric(do.call(rbind,strsplit(nodes,"p"))[,2])))
-    }
-    return(nodes)
+    as.character(na.omit(names(dfs(ig, node.id, "out" ,unreach = F)$order)))
 }
