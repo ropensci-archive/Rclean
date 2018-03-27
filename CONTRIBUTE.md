0f8507b6a1df1fd74333c4f5e696ae4df3029a2b
mklau <matthewklau@fas.harvard.edu>
2018-03-27 09:32:53 -0400

    adds CONTRIBUTE

---
+++ CONTRIBUTE.md
@@ -0,0 +1,27 @@
+Want to contribute to Rclean?
+=============================
+
+Great, thank you!
+
+# Issues
+
+First, just using the package and giving feedback is contributing. If
+the package is not working as described, congratulations you've
+discovered a bug! If you think this is the case, please submit an
+issue to the github
+[issue](https://github.com/ProvTools/Rclean/issues) system. 
+
+Include a reproducible example in the issue or a link to a
+[gist](https://gist.github.com/), with the following:
+
+1. Succinct title and description of expected results 
+2. Code with library dependencies and representative data for input
+3. Observed results description
+
+Last, please check that the example runs in a fresh R session before
+submitting. 
+
+# Enhancements
+
+If you haven't run into a bug, but would like the package to do
+something that it currently doesn't, you're welcome to 
--- R/parse.info.R
+++ R/parse.info.R
@@ -1,3 +1,24 @@
+# Copyright (C) 2017 Harvard University, Mount Holyoke College
+#
+# This file is part of Rclean
+#
+# Rclean is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# Rclean is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rclean; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Contact: Matthew Lau <matthewklau@fas.harvard.edu>
+
+
 #' .parse.info --- Parse node information from PROV-JSON.
 #' OUTPUT = A matrix of node information.
 #' 
