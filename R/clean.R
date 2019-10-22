44d3ec47a2ff4af8d0194731effb691f5884c96b
mklau <matthewklau@fas.harvard.edu>
2019-10-22 18:36:45 -0400

    refactor clean, moves some functions to clean.R

--- R/clean.R
+++ R/clean.R
@@ -79,18 +79,76 @@ clean <- function(file, vars, format = TRUE){
 #' @author Matthew K. Lau
 min_code <- function(src = "script", vars = "variables") {
     ## Define the lineage for all variables
-    lapply(vars, var_lineage, 
     vl <- var_lineage(src)
+    test <- lapply(vars, get_path, g = vl[["g"]])
     ## Find the lines and variables for all vars
     vp <- sapply(vars, get_path, g = vl[["g"]])
-    vp <- unique(unlist(vp))
-    ## Subest the graph to only required nodes
-    min.g <- vl[["g"]][rownames(vl[["g"]]) %in% vp, 
-                       colnames(vl[["g"]]) %in% vp]
-    cl <- as.character(unique(vl[["vdf"]][, "step"]))
-    vp <- vp[vp %in% cl]
-    vp <- sort(as.numeric(vp))
-    out <- as.character(src[vp])
+    ## Subest graphs with only required nodes for each var
+    g.min <- lapply(vp, min_graph, vl = vl)
+    ## Create a set list of lines for subsetting
+    l.min <- rownames(g.min)[is_number(rownames(g.min))]
+    ## Subsest the code to the minimum lines
+    out <- as.character(src[as.numeric(l.min)])
     return(out)
 }
 
+#' min_graph --- Minimize a graph to a list of paths.
+min_graph <- function(vp = "variable path", vl = "variable lineage") {
+    g.min <- vl[["g"]]
+    nodes <- unique(unlist(vp))
+    g.min <- g.min[rownames(g.min) %in% nodes, colnames(g.min) %in% nodes]
+    return(g.min)
+}
+
+#' get_path --- Lineage pathway for a given variable.
+#'
+#' Isolates the lineage for an variable. This can be either all steps
+#' leading to the creation of a variable or all descendent steps that
+#' use that variable.
+#' 
+#' @param g An adjacenty matrix that presents the use and creation of
+#'     variables by steps (i.e. complexes of function and operation
+#'     calls).
+#'@param node.id The name of the variable of interest.
+#'@param direction Determines the direction of searching on the graph,
+#'     either "in" (path leading to a variable) or "out" (path leading
+#'     from a variable).
+#'@return A character vector of steps and objects found along the path
+#'     for the object identified by the node ID.
+#'@importFrom igraph graph_from_adjacency_matrix
+#'@importFrom igraph dfs
+#'@noRd
+#'@author Matthew K. Lau
+
+get_path <- function(g = "graph", 
+                     node.id, 
+                     direction = "in") {
+    if (mode(g) == "list" & any(names(g) == "g")){
+        g <- g[["g"]]
+    }
+    if (missing(node.id)){
+        warning("Please supply a node name.")
+        print("Possible node names:", quote = FALSE)
+        rownames(g)[!(is_number(rownames(g)))]
+    }else{
+        ig <- graph_from_adjacency_matrix(g)
+        dfs.result <- dfs(ig, node.id, 
+                       direction, 
+                       unreachable = FALSE)$order
+        out <- as.character(na.omit(names(dfs.result)))
+        if (direction == "in"){out <- out[length(out):1]}
+    }
+return(out)
+}
+
+## is_number --- Detect numbers in a character vector of variables.
+is_number <- function(x) {
+    out <- logical()
+    for (i in seq_along(x)) {
+        out[i] <- !(any(
+               strsplit(x[i], split = "")[[1]] %in% 
+               c(letters, LETTERS)
+           ))
+    }
+    return(out)
+}
