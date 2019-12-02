c1bc2fe3e42f3f42064521f72347cfc12b23e7f6
mklau <matthewklau@fas.harvard.edu>
2019-12-02 18:47:44 -0500

    Removes format from data descriptions

--- R/long_script.R
+++ R/long_script.R
@@ -2,9 +2,4 @@
 #'
 #' Example script that is used in the Rclean examples.
 #'
-#' @format A vector of code lines:
-#' \describe{
-#'   \item{simple_script}{The script.}
-#'   ...
-#' }
 "long_script"
