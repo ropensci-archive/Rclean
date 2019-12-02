0fa031b78cb1b57ba77d8b549cb563c863b9450f
mklau <matthewklau@fas.harvard.edu>
2019-12-02 17:36:03 -0500

    Adds long and simple scripts as data files and roxygenizes

---
+++ R/simple_script.R
@@ -0,0 +1,9 @@
+#' Simple R script.
+#'
+#' Example script that is used in the Rclean examples.
+#'
+#' @format A vector of code lines.
+#' \describe{
+#'   \item{simple_script}{The script.}
+#' }
+"simple_script"
