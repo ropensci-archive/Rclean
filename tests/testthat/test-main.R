3c77990d0f8612a4e0574383b34169fa31fbffaa
mklau <matthewklau@fas.harvard.edu>
2019-12-03 18:38:28 -0500

    Adds tests for get_vars, code_graph, and keep

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -32,7 +32,7 @@ test_that("clean multiple variables", {
     expect_true(all(clean.multi == clean.multi.test))
 })
 
-test_that("codeGraph", {
+test_that("codeGraph as file", {
     load("codeGraph.src.rda")
     simple.script <- system.file(
         "example", "simple_script.R", package = "Rclean")
@@ -40,6 +40,25 @@ test_that("codeGraph", {
         code_graph(simple.script)
     )
     expect_true(length(output.cap) == 0)
+})
+
+test_that("codeGraph as script", {
+    load("codeGraph.src.rda")
+    simple.script <- system.file(
+        "example", "simple_script.R", package = "Rclean")
+    simple.script <- readScript(simple.script)
+    output.cap <- capture.output(
+        code_graph(simple.script)
+    )
+    expect_true(length(output.cap) == 0)
+})
+
+test_that("codeGraph write pdf", {
+    simple.script <- system.file(
+        "example", "simple_script.R", package = "Rclean")
+    code_graph(simple.script, pdf.file = "code_graph_test.pdf")
+    expect_true(file.exists("code_graph_test.pdf"))
+    file.remove("code_graph_test.pdf")
 })
 
 
@@ -51,10 +70,26 @@ test_that("keep write", {
     expect_true(all(keep.test == clean.simple.out))
 })
 
+test_that("get_vars as path", {
+    load("vars.out.rda")
+    script <- system.file(
+        "example", 
+        "simple_script.R", 
+        package = "Rclean")
+    vars.test <- get_vars(script)
+    expect_true(all(vars.test == vars.out))
+})
 
-## test_that("keep copy", {
-##     load("keep.test.rda")    
-##     keep(clean.simple.out)
-##     keep.test <- clipr::read_clip(allow_non_interactive = TRUE)
-##     expect_true(all(keep.test == clean.simple.out))
-## })
+test_that("get_vars as script", {
+    load("vars.out.rda")
+    script <- system.file(
+        "example", 
+        "simple_script.R", 
+        package = "Rclean")
+    script <- readScript(script)
+    vars.test <- get_vars(script)
+    expect_true(all(vars.test == vars.out))
+})
+
+
+
